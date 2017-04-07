6d85f1f07660c34173ae43860ca6c2026ff62387
huangzhongzhang <hzz1989@vip.qq.com>
2017-04-07 15:26:11 +0800

    更新自定义数据模块

--- login.pl
+++ login.pl
@@ -108,7 +108,7 @@ $client->load("MobileInfo");
 #示例：手机 1888888888
 
 #创建知识库
-$client->load("KnowledgeBase2",data=>{
+$client->load("KnowledgeBase",data=>{
         allow_group     => [ "PERL学习交流" ], # 可选，允许插件的群，可以是群名称或群号码
         ban_group       => [ "私人群", 123456 ], # 可选，禁用该插件的群，可以是群名称或群号码
         file            => './KnowledgeBase2.txt', # 数据库保存路径，纯文本形式，可以编辑
