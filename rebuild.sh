62ffbf2bd2c629170c3c37ca522f0221e6ee883e
huangzhongzhang <hzz1989@vip.qq.com>
2017-10-16 10:16:16 +0800

    添加rebuild脚本。

--- rebuild.sh
+++ rebuild.sh
@@ -5,4 +5,3 @@ docker rm Mojo-Webqq;
 docker build -t mojo-webqq .;
 docker images|grep none|awk '{print $3 }'|xargs docker rmi;
 docker run -t --name Mojo-Webqq --env MOJO_WEBQQ_LOG_ENCODING=utf8 -p 5011:5011 -v /tmp:/tmp -v /root:/root mojo-webqq;
-tail -f /tmp/nohup.out;
